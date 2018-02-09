37a539ed84ed5184fe3d9b730948665595cc70a4
hamfree <hamfree@users.noreply.github.com>
2018-02-09 20:14:00 +0100

    Se añaden unos ejemplos sobre entrada en consola desde javapractices.com

---
+++ Pruebas/src/name/ruiz/juanfco/maspruebas/ListaDeNumeros.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package name.ruiz.juanfco.maspruebas;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ListaDeNumeros {
+
+    private final List<Integer> lista;
+    private static final int CANTIDAD = 10;
+
+    public ListaDeNumeros() {
+        lista = new ArrayList<>(CANTIDAD);
+        for (int i = 0; i < CANTIDAD; i++) {
+            lista.add(i);
+        }
+    }
+
+    public void escribeLista() {
+        // El constructor FileWriter lanza IOException, que debe ser capturada.
+        try (PrintWriter salida = new PrintWriter(new FileWriter("ficheroSalida.txt"))) {
+            for (int i = 0; i < CANTIDAD; i++) {
+                // El método get(int) lanza IndexOutOfBoundsException, que debe ser capturada.
+                salida.println("Valor en: " + i + " = " + lista.get(i));
+            }
+            salida.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ListaDeNumeros.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
