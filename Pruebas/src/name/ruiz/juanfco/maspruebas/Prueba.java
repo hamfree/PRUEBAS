043a3d1dc0b840bb32421930eea85a891a16f846
hamfree <hamfree@users.noreply.github.com>
2018-02-09 20:14:00 +0100

    Se añaden unos ejemplos sobre entrada en consola desde javapractices.com

---
+++ Pruebas/src/name/ruiz/juanfco/maspruebas/Prueba.java
@@ -0,0 +1,135 @@
+package name.ruiz.juanfco.maspruebas;
+
+public class Prueba {
+
+    public static void main(String[] args) {
+        Prueba prueba = new Prueba();
+        A a = new A("Creado por el constructor", 0);
+
+        System.out.println("a = " + a.toString());
+        prueba.metodo1(a);
+        System.out.println("a = " + a.toString());
+
+        /*
+            Mientras no asignemos el valor devuelto por el método al objeto no lo 
+            cambiaremos por otro nuevo asignado dentro del método, pero sí se 
+            pueden modificar sus propiedades....
+         */
+        prueba.metodo2(a);
+        System.out.println("a = " + a.toString());
+
+        a = prueba.metodo2(a);
+        System.out.println("a = " + a.toString());
+
+        /*
+            Tanto metodo3() como metodo5() consiguen cambiar las propiedades de 
+            la instancia a, ya sea que devuelvan la 
+            instancia a o no devuelvan nada.
+         */
+        prueba.metodo3(a, "modificado en metodo 3", 3);
+        System.out.println("a = " + a.toString());
+
+        a = prueba.metodo4(a, "modificado en metodo 5", 5);
+        System.out.println("a = " + a.toString());
+
+        // Sin asignar el resultado, metodo4() no consigue que a sea null...
+        prueba.metodo5(a);
+        System.out.println("a = " + a.toString());
+
+        // Asignando el resultado, metodo4() hace que a sea null...
+        a = prueba.metodo5(a);
+        if (a != null) {
+            System.out.println("a = " + a.toString());
+        } else {
+            System.out.println("a = null");
+        }
+
+    }
+
+    /**
+     * Asigna una nueva instancia de A con parametros nulos a la instancia de A que se le pasa
+     *
+     * @param a la instancia de A a la que se quiere asignar una nueva instancia de A con parámetros nulos
+     */
+    public void metodo1(A a) {
+        System.out.println("--> metodo1()");
+        if (a != null) {
+            System.out.println("\ta = " + a.toString());
+            a = new A(null, null);
+            System.out.println("\ta = " + a.toString());
+        }
+        System.out.println("<-- metodo1()");
+    }
+
+    /**
+     * Asigna una nueva instancia de A con parametros nulos a la instancia de A que se le pasa, devolviendo de nuevo la instancia
+     * modificada
+     *
+     * @param a instancia de A a la que se quiere asignar una nueva instancia de A con parámetros nulos
+     * @return una instancia de A con los parámetros nulos
+     */
+    public A metodo2(A a) {
+        System.out.println("--> metodo2()");
+        if (a != null) {
+            System.out.println("\ta = " + a.toString());
+            a = new A(null, null);
+            System.out.println("\ta = " + a.toString());
+        }
+        System.out.println("<-- metodo2()");
+        return a;
+    }
+
+    /**
+     * Cambia los parámetros de la instancia de A.
+     *
+     * @param a la instancia de A a la que se modifican sus propiedades 'cadena' y 'entero'
+     * @param s la nueva String que contendrá la propiedad 'cadena' de la instancia de A apuntada por a
+     * @param i el nuevo Integer que contendrá la propiedad 'entero' de la instancia de A apuntada por a
+     */
+    public void metodo3(A a, String s, Integer i) {
+        System.out.println("--> metodo3()");
+        if (a != null) {
+            System.out.println("\ta = " + a.toString());
+            a.setCadena(s);
+            a.setEntero(i);
+            System.out.println("\ta = " + a.toString());
+        }
+        System.out.println("<-- metodo3()");
+    }
+
+    /**
+     * Cambia los parámetros de una instancia de A, devolviéndola al finalizar.
+     *
+     * @param a la instancia de A a la que se modifican sus propiedades 'cadena' y 'entero'
+     * @param s la nueva String que contendrá la propiedad 'cadena' de la instancia de A apuntada por a
+     * @param i el nuevo Integer que contendrá la propiedad 'entero' de la instancia de A apuntada por a
+     * @return una instancia de A con los parámetros cambiados
+     */
+    public A metodo4(A a, String s, Integer i) {
+        System.out.println("--> metodo4()");
+        if (a != null) {
+            System.out.println("\ta = " + a.toString());
+            a.setCadena(s);
+            a.setEntero(i);
+            System.out.println("\ta = " + a.toString());
+        }
+        System.out.println("<-- metodo4()");
+        return a;
+    }
+
+    /**
+     * Hace que instancia de A pasada como argumento apunte a null.
+     *
+     * @param a
+     * @return una instancia de A que apunta a null.
+     */
+    public A metodo5(A a) {
+        System.out.println("--> metodo5()");
+        System.out.println("\ta = " + a.toString());
+        a = null;
+        System.out.println("\ta = null");
+        System.out.println("<-- metodo5()");
+        return a;
+    }
+
+}
---
+++ Pruebas/src/name/ruiz/juanfco/pruebas/consola/ConsoleApp.java
@@ -0,0 +1,57 @@
+package name.ruiz.juanfco.pruebas.consola;
+
+import java.io.Console;
+import java.util.Arrays;
+
+/**
+ * Aplicación simple de consola interactiva. Usa la clase java.io.Console de *
+ * Java 6.
+ */
+public final class ConsoleApp {
+
+    public static final void main(String... aArgs) {
+        Console consola = System.console();
+        //lee el nombre de usuario, usando la sintaxis de java.util.Formatter:
+        String nombreusuario = consola.readLine("¿Nombre de Usuario? ");
+
+        //lee la contraseña, sin mostrar la salida
+        char[] claveacceso = consola.readPassword("¿Contraseña? ");
+
+        //verifica el nombre del usuario y la contrasena usando algún mecanismo
+        //(suprimido). La documentación java para la clase Console recomienda
+        //"poner a ceros" la contraseña cuando se termine la verificación.
+        Arrays.fill(claveacceso, ' ');
+
+        consola.printf("Bienvenido, %1$s.", nombreusuario);
+        consola.printf(NUEVA_LINEA);
+
+        String className = consola.readLine("Por favor, introduzca un nombre de "
+                + "clase completamente cualificado:");
+        Class laClase = null;
+        try {
+            laClase = Class.forName(className);
+            consola.printf("El árbol de herencia es: %1$s", getInheritanceTree(laClase));
+        } catch (ClassNotFoundException ex) {
+            consola.printf("No puedo encontrar esta clase.");
+        }
+
+        //esta versión simplemente sale, sin preguntar al usuario por más entradas
+        consola.printf("Adios.");
+    }
+
+    // PRIVADO
+    private static final String NUEVA_LINEA = System.getProperty("line.separator");
+
+    private static String getInheritanceTree(Class aClass) {
+        StringBuilder superclasses = new StringBuilder();
+        superclasses.append(NUEVA_LINEA);
+        Class theClass = aClass;
+        while (theClass != null) {
+            superclasses.append(theClass);
+            superclasses.append(NUEVA_LINEA);
+            theClass = theClass.getSuperclass();
+        }
+        superclasses.append(NUEVA_LINEA);
+        return superclasses.toString();
+    }
+}
