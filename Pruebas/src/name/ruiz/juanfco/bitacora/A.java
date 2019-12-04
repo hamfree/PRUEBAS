5d01011d21817e87050ad52bad6bb98f1ef07111
Juan F. Ruiz <juanfco.ruiz@gmail.com>
2019-12-04 12:48:36 +0100

    Se agregan tres proyectos, se actualizan los ignores y se borra un fichero manifest

---
+++ Pruebas/src/name/ruiz/juanfco/bitacora/A.java
@@ -0,0 +1,64 @@
+package name.ruiz.juanfco;
+
+public class A {
+	private String nombre;
+
+	/**
+	 * 
+	 */
+	public A() {
+		super();
+	}
+
+	/**
+	 * @param nombre
+	 */
+	public A(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		A other = (A) obj;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("A [nombre=").append(nombre).append("]");
+		return builder.toString();
+	}
+
+	
+
+	
+}
