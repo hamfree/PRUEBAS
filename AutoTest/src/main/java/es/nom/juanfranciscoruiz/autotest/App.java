ce4811a508387f0257ece91bdcf5240f569fa585
Juan F. Ruiz <juanfco.ruiz@outlook.es>
2021-05-25 22:04:22 +0200

    - Lo que hace la aplicacion no tiene mucha importancia. Es como se   hace el pom.xml para generar el JAR.

--- AutoTest/src/main/java/es/nom/juanfranciscoruiz/autotest/App.java
+++ AutoTest/src/main/java/es/nom/juanfranciscoruiz/autotest/App.java
@@ -1,29 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package es.nom.juanfranciscoruiz.autotest;
 
 import es.nom.juanfranciscoruiz.autotest.util.Directorio;
 
 /**
- *
+ * Punto de entrada de la aplicacion 'AutoTest' que muestra por pantalla
+ * los argumentos que se le pasan y también en que directorio se encuentra 
+ * la clase 'Directorio'.
+ * 
  * @author hamfree
  */
 public class App {
 
+    /**
+     * Entrada de la aplicación
+     * @param args matriz de argumentos que se le pueden pasar al programa (opcional)
+     */
     public static void main(String[] args) {
         App app = new App();
         app.procesa(args);
         System.exit(0);
     }
 
+    /**
+     * Realiza la función de la aplicación.
+     * @param args Los argumentos de la matriz de cadenas que se pasan al programa
+     */
     void procesa(String[] args) {
         testMuestraArgumentos(args);
         testDirectorioActual();
+        testDirectorioApp();
     }
 
+    /**
+     * Muestra por la salida estandar los argumentos que se le pasan al programa, 
+     * si así se ha hecho.
+     * @param args Los argumentos de la matriz de cadenas que se pasan al programa
+     */
     void testMuestraArgumentos(String[] args) {
         StringBuilder sb = new StringBuilder();
         if (args != null && args.length > 0) {
@@ -37,13 +49,29 @@ public class App {
         }
     }
 
+    /**
+     * Muestra el directorio donde se encuentra ubicada la clase Directorio
+     */
     void testDirectorioActual() {
         StringBuilder sb = new StringBuilder();
-        String ruta = Directorio.get().getAbsolutePath();
+        Directorio dir = new Directorio();
+        String rutaDirectorio = dir.get().getAbsolutePath();
         sb.setLength(0);
         sb.append("Directorio trabajo de la clase '")
                 .append(Directorio.class.getSimpleName()).append("' :")
-                .append(ruta);
+                .append(rutaDirectorio);
         System.out.println(sb.toString());
     }
+    
+    void testDirectorioApp() {
+        StringBuilder sb = new StringBuilder();
+        Directorio dir = new Directorio();
+        String rutaDirectorio = dir.get(this.getClass()).getAbsolutePath();
+        sb.setLength(0);
+        sb.append("Directorio trabajo de la clase '")
+                .append(App.class.getSimpleName()).append("' :")
+                .append(rutaDirectorio);
+        System.out.println(sb.toString());
+    }
+    
 }
