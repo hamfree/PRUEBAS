8782110d903e793704327c6bd7ef4b19208db6b3
Juan F. Ruiz <juanfco.ruiz@outlook.es>
2021-05-25 23:15:38 +0200

    - Se da mas informacion sobre las caracteristicas de la JVM.

---
+++ PruebaMaven/src/test/java/es/nom/juanfranciscoruiz/pruebamaven/AppTest.java
@@ -0,0 +1,20 @@
+package es.nom.juanfranciscoruiz.pruebamaven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
