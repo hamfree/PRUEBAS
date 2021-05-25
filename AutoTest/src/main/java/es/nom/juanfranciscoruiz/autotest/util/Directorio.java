5f5a1545257828d2ed69d6c7a63bfdcf2554f4ab
Juan F. Ruiz <juanfco.ruiz@outlook.es>
2021-05-25 22:04:22 +0200

    - Lo que hace la aplicacion no tiene mucha importancia. Es como se   hace el pom.xml para generar el JAR.

--- AutoTest/src/main/java/es/nom/juanfranciscoruiz/autotest/util/Directorio.java
+++ AutoTest/src/main/java/es/nom/juanfranciscoruiz/autotest/util/Directorio.java
@@ -6,47 +6,31 @@
 package es.nom.juanfranciscoruiz.autotest.util;
 
 import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 import java.net.URL;
 
 /**
+ * Obtiene el directorio donde se encuentra la clase que se le pase como argumento.
  *
  * @author hamfree
  */
 public class Directorio {
 
-    private static File DIRECTORIO_TRABAJO;
-    private static final String FILE = "file";
-    private static final String JAR = "jar";
-    private static final String EXT = ".class";
+  private File DIRECTORIO_TRABAJO;
 
-    public static File get() {
-        String recurso = Directorio.class.getSimpleName() + EXT;
-        if (DIRECTORIO_TRABAJO == null) {
-            try {
-                URL url = Directorio.class.getResource(recurso);
-                if (url.getProtocol().equals(FILE)) {
-                    File f = new File(url.toURI());
-                    do {
-                        f = f.getParentFile();
-                    } while (!f.isDirectory());
-                    DIRECTORIO_TRABAJO = f;
-                } else if (url.getProtocol().equals(JAR)) {
-                    String esperado = "!/" + recurso;
-                    String s = url.toString();
-                    s = s.substring(4);
-                    s = s.substring(0, s.length() - esperado.length());
-                    File f = new File(new URL(s).toURI());
-                    do {
-                        f = f.getParentFile();
-                    } while (!f.isDirectory());
-                    DIRECTORIO_TRABAJO = f;
-                }
-            } catch (MalformedURLException | URISyntaxException e) {
-                DIRECTORIO_TRABAJO = new File(".");
-            }
-        }
-        return DIRECTORIO_TRABAJO;
+  public File get() {
+    DIRECTORIO_TRABAJO = get(Directorio.class);
+    return DIRECTORIO_TRABAJO;
+  }
+
+  public File get(Class clase) throws IllegalArgumentException {
+    URL ubicacion;
+    if (clase != null) {
+      ubicacion = clase.getProtectionDomain().getCodeSource().getLocation();
+    } else {
+      throw new IllegalArgumentException("¡Parametro nulo!");
     }
+    DIRECTORIO_TRABAJO = new File(ubicacion.getFile());
+
+    return DIRECTORIO_TRABAJO;
+  }
 }
